9e64f11b614b768253b46346a239f91960c76b84
Daniel George <120543571+datcuriousguy@users.noreply.github.com>
2025-06-04 22:17:57 +0530

    Update relevance_checker.py
    Added score is a fraction as explained (1-missing)

--- relevance_checker.py
+++ relevance_checker.py
@@ -1,7 +1,11 @@
+
 """
 This function accepts the resume's text and the keywords from the api
 and splits the words into two lists:
 
 1. matched
 2. missing
+
+the score object is simply the fraction of matching keywords from the
+resume. The missing one can be thought of as "1 - matching".
 """
