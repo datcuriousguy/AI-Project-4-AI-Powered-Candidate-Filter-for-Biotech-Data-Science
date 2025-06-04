82fa0c0475487f07728ca99048f763dd713f4073
Daniel George <120543571+datcuriousguy@users.noreply.github.com>
2025-06-04 22:35:06 +0530

    Update resume_parser.py
    planning out the functions argument

--- resume_parser.py
+++ resume_parser.py
@@ -3,4 +3,6 @@ import pdfplumber
 """
 This is the function to get pdf text using python's promising
 pdfplumber module.
+
+As you can guess, file_path is the path to the resume to be examined.
 """
