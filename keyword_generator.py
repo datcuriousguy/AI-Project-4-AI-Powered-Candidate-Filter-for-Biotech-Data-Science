8aadc808a822efcb73fc55d0340f635a7cdf8633
Daniel George <120543571+datcuriousguy@users.noreply.github.com>
2025-06-05 14:54:51 +0530

    Create keyword_generator.py
    creating file

---
+++ keyword_generator.py
@@ -0,0 +1,7 @@
+
+"""
+Here, we will write code to use openai's relevant API to get relevant
+keywords using a prompt that is designed to ask for specific keywords for
+a bio-meets-datascience role. Kind of exxcited to see what it'lll
+return!
+"""
