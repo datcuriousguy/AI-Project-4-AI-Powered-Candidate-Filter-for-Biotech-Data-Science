978fab3d23051e636a596471f9e1b0b950d1db06
Daniel George <120543571+datcuriousguy@users.noreply.github.com>
2025-06-06 11:58:02 +0530

    Update keyword_generator.py
    The first response!:
    Q. What's the key to becoming a good Data Scientist?
    
    A. The Key is to Be Responsive. Responsiveness is in control. The world of business has changed. The focus has changed. This is what has changed. Every business has changed. The Data Science field is about getting people to use the data.
    
    Q. I can't stay on my team in our organization for
    
    LOL!

--- keyword_generator.py
+++ keyword_generator.py
@@ -32,3 +32,17 @@ Note the 0 index isn't really necessary as its just one response.
 print(response[0]["generated_text"])
 
 
+# Note: this was the first response!!!
+#
+# Device set to use cpu
+# Truncation was not explicitly activated but `max_length` is provided a specific value, please use `truncation=True` to explicitly truncate examples to max length. Defaulting to 'longest_first' truncation strategy. If you encode pairs of sequences (GLUE-style) with the tokenizer you can select this strategy more precisely by providing a specific strategy to `truncation`.
+# Setting `pad_token_id` to `eos_token_id`:50256 for open-end generation.
+# List 15 key technical skills for a Data Scientist in Biotechnology. Read more.
+#
+# Q. What's the key to becoming a good Data Scientist?
+#
+# A. The Key is to Be Responsive. Responsiveness is in control. The world of business has changed. The focus has changed. This is what has changed. Every business has changed. The Data Science field is about getting people to use the data.
+#
+# Q. I can't stay on my team in our organization for
+#
+# Process finished with exit code 0
