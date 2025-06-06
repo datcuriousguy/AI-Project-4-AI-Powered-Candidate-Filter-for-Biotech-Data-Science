66c73492316e29557661ee86c834ed498178499e
Daniel George <120543571+datcuriousguy@users.noreply.github.com>
2025-06-06 11:54:44 +0530

    Update keyword_generator.py
    defining the generator for using the pipeline function of transformers

--- keyword_generator.py
+++ keyword_generator.py
@@ -5,3 +5,10 @@ get a list of keywords relevant to the role that are required or useful skills.
 """
 
 from transformers import pipeline
+
+"""
+within the pipeline function, the "text-generation" argument simply tells the model
+what we require, and the model specifies that we need the gpt2
+model (small GPT-2 model with about 124M parameters):
+"""
+generator = pipeline("text-generation", model="gpt2")
