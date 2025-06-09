27588669fc2a8e1739567fa70fb4c7b34944ac29
Daniel George <120543571+datcuriousguy@users.noreply.github.com>
2025-06-09 19:17:11 +0530

    Update keyword_generator.py
    switching to yake for keywords

--- keyword_generator.py
+++ keyword_generator.py
@@ -4,31 +4,8 @@ Here, we implement a functuion using the transformers pipeline to
 get a list of keywords relevant to the role that are required or useful skills.
 """
 
-from transformers import pipeline
 
 """
-within the pipeline function, the "text-generation" argument simply tells the model
-what we require, and the model specifies that we need the gpt2
-model (small GPT-2 model with about 124M parameters):
+Going to try out using yake for keyword compilation, as gpt2
+went to the bar this weekend.
 """
-generator = pipeline("text-generation", model="sshleifer/tiny-gpt2")
-
-"""this is what we are inputting into the model, the prompt. its asking for the relevant keywords:"""
-
-prompt = "List 15 key technical skills for a Data Scientist in Biotechnology."
-
-"""
-asks for the output to be a minimum of 100 tokens (like 75-100 words) and
-num_return_sequences = 1 simply asks the model to generate just one
-response (many are possible.)
-"""
-
-response = generator(prompt, max_length=100, num_return_sequences=1)
-
-"""
-Note the 0 index isn't really necessary as its just one response.
-"""
-
-print(response[0]["generated_text"])
-
-#
