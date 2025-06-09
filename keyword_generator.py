8fe0fa264566d0fed79afd61877e2e2f38e417c3
Daniel George <120543571+datcuriousguy@users.noreply.github.com>
2025-06-09 19:18:42 +0530

    Update keyword_generator.py
    importing yake

--- keyword_generator.py
+++ keyword_generator.py
@@ -4,8 +4,9 @@ Here, we implement a functuion using the transformers pipeline to
 get a list of keywords relevant to the role that are required or useful skills.
 """
 
-
 """
 Going to try out using yake for keyword compilation, as gpt2
 went to the bar this weekend.
 """
+
+import yake
